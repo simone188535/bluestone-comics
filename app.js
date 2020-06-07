e37b344f4370a9efafb8166c95bffb315cab901c
Simone Anthony <simoneanthony@Simones-MacBook-Pro.local>
2020-06-06 21:33:51 -0400

    more edits

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ const morgan = require('morgan');
 
 // 1) Middlewares
 app.use(express.json());
-app.use(morgan('dev'));
-app.use('/api/v1/users', userRoutes);
 
-// 2) start server
+app.use(morgan('dev'));
+
+//2) Routes
+app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/read', () => {
+console.log('testing');
+});
+
+// 3) start server
 const port = 5000;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
