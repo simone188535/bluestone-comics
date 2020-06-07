c86b4efdf14b2d281570980080558dfe624ff27d
Simone Anthony <simoneanthony@Simones-MacBook-Pro.local>
2020-06-06 22:33:52 -0400

    env variables added and file seperation

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-const dotenv = require('dotenv');
 
 const userRoutes = require('./routes/userRoutes');
 
-// 1) Middlewares
-app.use(express.json());
 
-app.use(morgan('dev'));
+// 1) Middlewares
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+console.log(process.env.NODE_ENV);
+app.use(express.json());
 
 //2) Routes
 app.use('/api/v1/users', userRoutes);
@@ -16,11 +18,4 @@ app.use('/api/v1/read', () => {
 console.log('testing');
 });
 
-// 3) start server
-// This enables ENV Variables
-dotenv.config({path: './config.env'});
-// console.log(process.env);
-const port = 5000;
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
-});
+module.exports = app;
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+const dotenv = require('dotenv');
+// This enables ENV Variables
+dotenv.config({path: './config.env'});
+
 const app = require('./app');
 
 // start server
-const port = 5000;
+const port = process.env.PORT || 5000;
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 });
-
