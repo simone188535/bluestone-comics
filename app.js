e3a9320b6819b42d445132c003015c088561c3c6
Simone Anthony <simoneanthony@Simones-MacBook-Pro.local>
2020-06-06 22:08:45 -0400

    dotenv added

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-const userRoutes = require('./routes/userRoutes');
 const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+const userRoutes = require('./routes/userRoutes');
 
 // 1) Middlewares
 app.use(express.json());
@@ -15,6 +17,9 @@ console.log('testing');
 });
 
 // 3) start server
+// This enables ENV Variables
+dotenv.config({path: './config.env'});
+// console.log(process.env);
 const port = 5000;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
