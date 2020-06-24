a911ecbe674c98af44128e51f650da8be65394d2
Simone Anthony <simoneanthony@Simones-MacBook-Pro.local>
2020-06-23 21:03:28 -0400

    plenty of edits

--- utils/email.js
+++ utils/email.js
@@ -1,33 +1,33 @@
 const nodemailer = require('nodemailer');
-const sgTransport = require('nodemailer-sendgrid-transport');
+const sgMail = require('@sendgrid/mail');
 const keys = require('../config/keys');
 const AppError = require('./appError');
 
 const sendEmail = async (options) => {
   // 1) Create a transporterOptions
-  const transporterOptions = {
-    auth: {
-      api_key: keys.SENDGRID_API_KEY
-    }
-  };
-  const client = nodemailer.createTransport(sgTransport(transporterOptions));
+  sgMail.setApiKey(keys.SENDGRID_API_KEY);
 
-  // 2) Define the email options
-  const mailOptions = {
+  const msg = {
     from: keys.EMAIL,
     to: options.email,
     subject: options.subject,
     text: options.text,
     html: '<strong>and easy to do anywhere, even with Node.js</strong>'
   };
+  sgMail.send(msg);
 
   // 3) Actually send the email
-  try {
-    await client.sendMail(mailOptions);
-    console.log('WORKING!!!!!');
-  } catch (err) {
-    new AppError('Email could not be sent', 503);
-  }
+  //   try {
+//   await client.sendMail(mailOptions, function (err, res) {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(res);
+//   });
+//   console.log('WORKING!!!!!');
+  //   } catch (err) {
+  //     new AppError('Email could not be sent', 503);
+  //   }
 };
 
 module.exports = sendEmail;
