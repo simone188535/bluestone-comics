b718d88ddd731c1b4a10021924c634560b3907fe
Simone Anthony <simoneanthony@Simones-MacBook-Pro.local>
2020-06-30 01:33:09 -0400

    email util debugged

--- utils/email.js
+++ utils/email.js
@@ -9,22 +9,32 @@ const sendEmail = async (options) => {
 
   const msg = {
     from: keys.EMAIL,
-    to: options.email,
+    to: 'simone.anthony1@yahoo.com',
     subject: options.subject,
     text: options.text,
     html: '<strong>and easy to do anywhere, even with Node.js</strong>'
   };
-  sgMail.send(msg);
+  sgMail.send(msg).then(
+    () => {
+      console.log('email sent');
+    },
+    (error) => {
+      console.error(error);
 
+      if (error.response) {
+        console.error(error.response.body);
+      }
+    }
+  );
   // 3) Actually send the email
   //   try {
-//   await client.sendMail(mailOptions, function (err, res) {
-//     if (err) {
-//       console.log(err);
-//     }
-//     console.log(res);
-//   });
-//   console.log('WORKING!!!!!');
+  //   await client.sendMail(mailOptions, function (err, res) {
+  //     if (err) {
+  //       console.log(err);
+  //     }
+  //     console.log(res);
+  //   });
+  //   console.log('WORKING!!!!!');
   //   } catch (err) {
   //     new AppError('Email could not be sent', 503);
   //   }
