a87e7753e6adc2378df00ebec2438a2b4dda70a7
Simone Anthony <simoneanthony@Simones-MacBook-Pro.local>
2020-07-17 21:42:32 -0400

    rename role

--- models/userModel.js
+++ models/userModel.js
@@ -31,7 +31,7 @@ const userSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    enum: ['user', 'admin'],
+    enum: ['user', 'admin', 'moderator', 'creator'],
     default: 'user'
   },
   photo: {
