9b396dbcfe26d306f6cd5612e1a9cc99d323dd96
Simone Anthony <simoneanthony@Simones-MacBook-Pro.local>
2020-07-22 02:34:02 -0400

    scss files init

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.scss';
 import { Provider } from 'react-redux';
 
 import App from './components/App';
