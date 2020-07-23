bb433ef51fd679dcfe1211f6feb50485fe06b811
Simone Anthony <simoneanthony@Simones-MacBook-Pro.local>
2020-07-22 21:26:48 -0400

    another bad commit message

--- client/src/components/auth/forms/LoginForm.js
+++ client/src/components/auth/forms/LoginForm.js
@@ -12,13 +12,14 @@ const onSubmit = values => {
 
 const validate = values => {
     let errors = {};
-    if (!values.name) {
-        errors.name = 'Required';
-    }
+    
     if (!values.email) {
         errors.email = 'Required';
     } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
         errors.email = 'Invalid email format';
+    }
+    if (!values.password) {
+        errors.password = 'Required';
     }
 
     return errors;
@@ -36,8 +37,10 @@ function LoginForm (props) {
             <form onSubmit={formik.handleSubmit}>
                 <label htmlFor="email">Email</label>
                 <input type="text" id="email" name="email" onChange={formik.handleChange} value={formik.values.email}/>
+                {formik.errors.email ? <div class="error">{formik.errors.email}</div> : null}
                 <label htmlFor="password">Password</label>
                 <input type="password" id="password" name="password" onChange={formik.handleChange} value={formik.values.password}/>
+                {formik.errors.password ? <div class="error">{formik.errors.password}</div> : null}
                 <button type="submit">Submit</button>
             </form>
         </div>
