c83d725d1d4318ebb4c4591929de3d6034672444
Simone Anthony <simoneanthony@Simones-MacBook-Pro.local>
2020-07-23 02:02:24 -0400

    login form created

--- client/src/components/auth/forms/LoginForm.js
+++ client/src/components/auth/forms/LoginForm.js
@@ -1,49 +1,65 @@
 import React from 'react';
-import { useFormik } from 'formik';
+import { useFormik, ErrorMessage } from 'formik';
 
-
-const initialValues = {
-    email: '',
-    password: ''
-}
 const onSubmit = values => {
     console.log('Form data', values)
 }
 
 const validate = values => {
-    let errors = {};
-    
+    const errors = {};
+
     if (!values.email) {
-        errors.email = 'Required';
+      errors.email = 'Required';
     } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-        errors.email = 'Invalid email format';
+      errors.email = 'Invalid email address';
     }
+
     if (!values.password) {
         errors.password = 'Required';
-    }
-
+      } 
+  
     return errors;
-}
-function LoginForm (props) {
-    const formik = useFormik({
-        initialValues,
-        onSubmit,
-        validate
-    });
+  };
+  
 
-    console.log('Form values', formik.errors);
-    return(
-        <div>
-            <form onSubmit={formik.handleSubmit}>
-                <label htmlFor="email">Email</label>
-                <input type="text" id="email" name="email" onChange={formik.handleChange} value={formik.values.email}/>
-                {formik.errors.email ? <div class="error">{formik.errors.email}</div> : null}
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password" name="password" onChange={formik.handleChange} value={formik.values.password}/>
-                {formik.errors.password ? <div class="error">{formik.errors.password}</div> : null}
-                <button type="submit">Submit</button>
-            </form>
-        </div>
-    );
+function LoginForm() {
+    const formik = useFormik({
+        initialValues: {
+          email: '',
+          password: ''
+        },
+        validate,
+        onSubmit
+      });
+
+      return (
+        <form onSubmit={formik.handleSubmit}>
+          <label htmlFor="email">Email Address</label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+          />
+          {formik.touched.email && formik.errors.email ? (
+            <div>{formik.errors.email}</div>
+          ) : null}
+        <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.lastName}
+          />
+          {formik.touched.lastName && formik.errors.lastName ? (
+            <div>{formik.errors.lastName}</div>
+          ) : null}
+          <button type="submit">Submit</button>
+        </form>
+      );
 }
 export default LoginForm;
