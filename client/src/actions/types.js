5803475c83c24f2f5a360a250226cac460add1e0
Simone Anthony <simoneanthony@Simones-MacBook-Pro.local>
2020-07-27 21:45:38 -0400

    npm installs and redux auth edits

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,1 +1,5 @@
+// Auth 
 export const FETCH_USER = 'fetch_user';
+export const LOGIN_SUCCESS = 'login_success';
+export const LOGIN_FAILURE = 'login_failure';
+export const LOGOUT_SUCCESS = 'logout_success';
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,9 @@
 import { FETCH_USER } from '../actions/types';
 
-export default function(state = null, action) {
+export default function(state = {
+    isFetching: false,
+    isAuthenticated: null
+  }, action) {
     switch (action.type) {
         case FETCH_USER: 
         return action.payload || false;
