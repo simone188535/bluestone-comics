07c9e6745e61b33e5de5b6f17cc1ebc65401b681
Simone Anthony <simoneanthony@Simones-MacBook-Pro.local>
2020-07-27 20:36:00 -0400

    server and client split

---
+++ server/config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  MONGO_URI: 'mongodb+srv://santhony:bluestonedev@bluestonecomics-dev-h6x8x.mongodb.net/bluestonecomics-dev?retryWrites=true&w=majority',
+  JWT_SECRET: 'uyg7rf65rUYGBygbsiyg6f5d4ws3wsv3ws',
+  EMAIL: 'no-reply@bluestonecomics.com',
+  SENDGRID_API_KEY: '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'
+};
