96d39b276284d0718df1e443543c27b28d4c27db
Simone Anthony <simoneanthony@Simones-MacBook-Pro.local>
2020-07-27 22:41:26 -0400

    authAction modified with new reducer edits

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { FETCH_USER } from './types';
+import { FETCH_USER, LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE, LOGOUT_SUCCESS } from './types';
 
 const fetchUser = (data) => {
     return { type: FETCH_USER, payload: data }
@@ -7,6 +7,9 @@ const fetchUser = (data) => {
 // This "nesting" is called currying (it also counts as a higher order function). Go here for more: https://stackoverflow.com/questions/32782922/what-do-multiple-arrow-functions-mean-in-javascript
 const login = (email, password) => async (dispatch) => {
   try {
+    
+    dispatch({ type: LOGIN_REQUEST });
+    
     const res = await axios.post('/api/v1/users/login', {
       email,
       password
@@ -14,10 +17,11 @@ const login = (email, password) => async (dispatch) => {
 
     localStorage.setItem('jwtToken', res.data.token);
 
-    dispatch({ type: FETCH_USER, payload: res.data.data.user });
+    dispatch({ type: LOGIN_SUCCESS, user: res.data.data.user });
   } catch (err) {
     console.log('!!!!!!!!!!error', err);
     //send redux error
+    dispatch({ type: LOGIN_FAILURE, error: '!!!THIS IS AN ERROR MESSAGE!!!' });
   }
 };
  const Signup = () => {
