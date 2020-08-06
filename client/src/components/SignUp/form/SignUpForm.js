9f68469d5e56366280eb9346ea3c6bf0af4666a7
Simone Anthony <simoneanthony@Simones-MacBook-Pro.local>
2020-08-05 20:15:07 -0400

    initial styling and creation of sign up form

--- client/src/components/SignUp/form/SignUpForm.js
+++ client/src/components/SignUp/form/SignUpForm.js
@@ -13,27 +13,62 @@ function SignUpForm() {
         dispatch(authActions.login(values.email, values.password));
         setSubmitting(false);
     }
-    
+
     return (
         <div>
             <Formik
-                initialValues={{ email: '', password: '' }}
+                initialValues={{ firstName: '', lastName: '', username: '', email: '', password: '', passwordConfirm: '' }}
                 validationSchema={Yup.object({
+                    firstName: Yup.string()
+                        .max(15, 'Must be 15 characters or less')
+                        .required('Required'),
+                    lastName: Yup.string()
+                        .max(20, 'Must be 20 characters or less')
+                        .required('Required'),
+                    username: Yup.string()
+                        .max(20, 'Must be 20 characters or less')
+                        .required('Required'),
                     email: Yup.string()
                         .email('Invalid email address')
                         .required('Required'),
                     password: Yup.string()
                         .required('Required'),
+                    passwordConfirm: Yup.string()
+                        .required('Required'),
                 })}
                 onSubmit={onSubmit}
             >
                 <Form>
-                    <label htmlFor="email">Email Address</label>
-                    <Field name="email" type="email" />
-                    <ErrorMessage name="email" />
-                    <label htmlFor="password">Password</label>
-                    <Field name="password" type="password" />
-                    <ErrorMessage name="password" />
+                    <div className="form-group">
+                        <label htmlFor="firstName">First Name: </label>
+                        <Field name="firstName" type="text" />
+                        <ErrorMessage name="firstName" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="lastName">Last Name: </label>
+                        <Field name="lastName" type="text" />
+                        <ErrorMessage name="lastName" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="username">Username: </label>
+                        <Field name="username" type="text" />
+                        <ErrorMessage name="username" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email Address: </label>
+                        <Field name="email" type="email" />
+                        <ErrorMessage name="email" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <Field name="password" type="password" />
+                        <ErrorMessage name="password" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="passwordConfirm">Password Confirm</label>
+                        <Field name="passwordConfirm" type="password" />
+                        <ErrorMessage name="passwordConfirm" />
+                    </div>
                     <button type="submit">Submit</button>
                 </Form>
             </Formik>
