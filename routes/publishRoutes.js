51828c06b568c7232193d967b374c10756dfece5
Simone Anthony <simoneanthony@Simones-MacBook-Pro.local>
2020-09-02 17:52:55 -0400

    corrections to publish routes

--- routes/publishRoutes.js
+++ routes/publishRoutes.js
@@ -6,20 +6,20 @@ const authController = require('../controllers/authController');
 const publishController = require('../controllers/publishController');
 
 // This middleware runs before all the routes beneath get the chance to. This checks if user is present for all routes before continuing.
-// router.use(authController.protect);
+router.use(authController.protect);
 // router.use(authController.restrictTo('creator'));
 
-router.route('/').post(authController.protect, publishController.createBook);
+router.route('/').post(publishController.createBook);
 router
   .route('/:urlSlug/book/:bookId')
   .get(publishController.getBook)
-  .post(authController.protect, publishController.createIssue)
-  .patch(authController.protect, publishController.updateBook)
-  .delete(authController.protect, publishController.deleteBook);
+  .post(publishController.createIssue)
+  .patch(publishController.updateBook)
+  .delete(publishController.deleteBook);
 router
   .route('/:urlSlug/book/:bookId/issue/:issueNumber')
-  // .get(publishController.getIssue)
-  .patch(authController.protect, publishController.updateIssue)
-  .delete(authController.protect, publishController.deleteIssue);
+  .get(publishController.getIssue)
+  .patch(publishController.updateIssue)
+  .delete(publishController.deleteIssue);
 
 module.exports = router;
