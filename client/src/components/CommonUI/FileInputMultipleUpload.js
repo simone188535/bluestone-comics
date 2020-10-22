56082e8b662ccf6d2b9740f30daa0b43920aaedb
Simone Anthony <simoneanthony@Simones-MacBook-Pro.local>
2020-10-21 21:55:28 -0400

    multifile upload image cross out button added

--- client/src/components/CommonUI/FileInputMultipleUpload.js
+++ client/src/components/CommonUI/FileInputMultipleUpload.js
@@ -1,5 +1,8 @@
 import React, { useState, useCallback, useEffect } from "react";
 import { useDropzone } from "react-dropzone";
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const FileInputMultipleUpload = ({ setFieldValue, identifier, className }) => {
     const [files, setFiles] = useState([]);
@@ -48,11 +51,24 @@ const FileInputMultipleUpload = ({ setFieldValue, identifier, className }) => {
         </li>
     ));
 
-    const onReorder = () => {
+    const onDragEnd = (result) => {
         /*
         This will allow for drag and dropping capabilities in the preview section. 
         setFieldValue needs to be reset. when this occurs.
+        
+        If you want to know how works, the drag and drop functionality was modelled after 
+        this article: https://www.freecodecamp.org/news/how-to-add-drag-and-drop-in-react-with-react-beautiful-dnd/
         */
+
+        // Preventing errors from dragging out of bounds
+        if (!result.destination) return;
+
+        const items = Array.from(files);
+        const [reorderedItem] = items.splice(result.source.index, 1);
+        items.splice(result.destination.index, 0, reorderedItem);
+
+        setFiles(items);
+
     }
     const removalOnClick = () => {
         /* 
@@ -71,21 +87,41 @@ const FileInputMultipleUpload = ({ setFieldValue, identifier, className }) => {
 
             </div>
             <div className="file-input-multiple-upload-preview-container">
-                <h4>Accepted files</h4>
-                <ul>{acceptedFileItems}</ul>
+                {/* <h4>Accepted files</h4>
+                <ul>{acceptedFileItems}</ul> */}
                 <h4>Rejected files</h4>
                 <ul>{fileRejectionItems}</ul>
                 <h4>Preview files</h4>
-                    <ul className="thumb-nails">
-                        {files.map(uploadedFile => (
-                            <li key={uploadedFile.name}>
-                                <img
-                                    src={uploadedFile.preview}
-                                />
-                            </li>
-                        )
+                <DragDropContext onDragEnd={onDragEnd}>
+                    <Droppable droppableId="thumb-nails">
+                        {(provided) => (
+                            <ul className="thumb-nails" {...provided.droppableProps} ref={provided.innerRef}>
+                                {files.map((uploadedFile, index) => (
+                                    // This maps the current file state and implements react-beautiful-dnd so that the images uploaded can be sorted in order
+                                    <Draggable key={uploadedFile.name} draggableId={uploadedFile.name} index={index}>
+                                        {(provided) => (
+                                            <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+
+                                                <button className="thumb-nail-remove-icon">
+                                                    <FontAwesomeIcon
+                                                        icon={faTimes}
+                                                        size="sm"
+                                                    />
+                                                </button>
+
+                                                <img className="thumb-nail-img"
+                                                    src={uploadedFile.preview}
+                                                />
+
+                                            </li>
+                                        )}
+                                    </Draggable>
+                                ))}
+                                {provided.placeholder}
+                            </ul>
                         )}
-                    </ul>
+                    </Droppable>
+                </DragDropContext>
             </div>
         </>
     )
