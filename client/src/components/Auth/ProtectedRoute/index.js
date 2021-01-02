e888393a3be5524c83dceaab6c90280f97a0f929
Simone Anthony <simoneanthony@Simones-MacBook-Pro.local>
2021-01-01 21:11:06 -0500

    protected route component

--- client/src/components/Auth/ProtectedRoute/index.js
+++ client/src/components/Auth/ProtectedRoute/index.js
@@ -2,12 +2,31 @@ import React, { useState } from 'react';
 import { useSelector } from "react-redux";
 import { Route, Redirect } from 'react-router-dom';
 
-const ProtectedRoute = ({ component: Component, ...rest}) => {
-    
+/* 
+For more info on how this works: 
+https://dev.to/mychal/protected-routes-with-react-function-components-dh
+https://www.youtube.com/watch?v=Y0-qdp-XBJg
+*/
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+
     const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
-    
+
+    console.log('isAuthenticated', isAuthenticated);
     return (
-        <Component />
+        // If the user is logged in, allow them to continue to the route, if not, redirect them to the login page.
+        <>
+            {
+                <Route {...rest} render={props => <Component {...rest} {...props} />} />
+                // (isAuthenticated) ?
+                //     <Route {...rest} render={props => <Component {...rest} {...props} />} />
+                //     :
+                //     <Redirect to={
+                //         {
+                //             pathname: '/login',
+                //         }
+                //     } />
+            }
+        </>
     );
 
 }
