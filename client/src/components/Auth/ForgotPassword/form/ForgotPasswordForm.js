5f72a814840f31cdda69d37aa91a68efbfcf90ad
Simone Anthony <simoneanthony@Simones-MacBook-Pro.local>
2021-01-07 22:47:37 -0500

    edits to forgot password

--- client/src/components/Auth/ForgotPassword/form/ForgotPasswordForm.js
+++ client/src/components/Auth/ForgotPassword/form/ForgotPasswordForm.js
@@ -3,30 +3,42 @@ import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from 'react-router-dom';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
-import { authActions } from "../../../../actions";
+import { AuthenticationServices } from '../../../../services';
 
 
 
 function ForgotPasswordForm() {
-    const history = useHistory();
-    // const dispatch = useDispatch();
+    // const history = useHistory();
+    const [statusMessage, setStatusMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState(false);
     const [enableMessage, setEnableMessage] = useState(false);
     // const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
     // const hasError = useSelector(state => state.error.hasError);
     // const errorMessage = useSelector(state => state.error.errorMessage);
 
-    // const authMessage = () => {
-    //     if (isAuthenticated) {
-    //         return <span className="success-text-color"> Login successful!</span>;
-    //     } else if (hasError) {
-    //         return <span className="error-text-color">{errorMessage} </span>;
-    //     } else {
-    //         return '';
-    //     }
-    // }
+    const authMessage = () => {
+
+        let textColor = (errorMessage) ? 'error-text-color' : 'success-text-color';
+       
+        return <span className={textColor}> {statusMessage} </span>;
+    }
 
     const onSubmit = async (values, { setSubmitting }) => {
-        // dispatch(authActions.login(values.email, values.password));
+        try {
+            await AuthenticationServices.forgotPassword(values.email);
+            
+            // unset error message if it is set.
+            if (errorMessage) {
+                setErrorMessage(false);
+            }
+
+            setStatusMessage('A reset token has been sent to your email.');
+
+        } catch (err) {
+            setStatusMessage(err.message);
+            setErrorMessage(true);
+        }
+        
         setEnableMessage(true);
         setSubmitting(false);
 
@@ -63,7 +75,7 @@ function ForgotPasswordForm() {
                     <button type="submit" className="form-submit form-item">Submit</button>
                 </Form>
             </Formik>
-            {/* <div className="forgot-password-status-message">{enableMessage && authMessage()}</div> */}
+            <div className="forgot-password-status-message">{enableMessage && authMessage()}</div>
         </div>
     );
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,9 +9,7 @@ const authController = require('../controllers/authController');
 router.route('/').get(authController.protect, userController.getAllUsers);
 router.route('/signup').post(authController.signup);
 router.route('/login').post(authController.login);
-router
-  .route('/forgot-password')
-  .post(authController.protect, authController.forgotPassword);
+router.route('/forgot-password').post(authController.forgotPassword);
 router
   .route('/reset-password/:token')
   .patch(authController.protect, authController.resetPassword);
