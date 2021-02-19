61c095dc9ff8c3bfae82a60cf3b781310c642bef
Simone Anthony <simoneanthony@Simones-MacBook-Pro.local>
2021-02-18 22:25:22 -0500

    subscribe, unsubscribe and check subscription complete for now

--- controllers/subscriberController.js
+++ controllers/subscriberController.js
@@ -2,17 +2,54 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const Subscriber = require('../models/subscriberModel');
 
-exports.Subscribe = catchAsync(async (req, res) => {
-  // const usernameQuery = req.query.q;
+exports.checkSubscription = (onlyCheckSubscription = false) =>
+  catchAsync(async (req, res, next) => {
+    const currentUserIsSubscribedToPublisher = await Subscriber.findOne({
+      publisher: req.body.publisher,
+      subscriber: res.locals.user.id
+    });
 
-  // const users = await User.find({
-  //   username: { $regex: usernameQuery, $options: 'i' }
-  // });
+    if (!onlyCheckSubscription) {
+      res.locals.subscriber = currentUserIsSubscribedToPublisher;
+      return next();
+    }
 
-  // Send Response
-  res.status(200).json({
-    // results: users.length,
-    // users,
+    res.status(200).json({
+      subscribed: currentUserIsSubscribedToPublisher,
+      status: 'success'
+    });
+  });
+
+exports.Subscribe = catchAsync(async (req, res, next) => {
+  if (res.locals.subscriber) {
+    return next(
+      new AppError('This user is already subscribed to this publisher.', 401)
+    );
+  }
+  const subscribeToPublisher = await Subscriber.create({
+    publisher: req.body.publisher,
+    subscriber: res.locals.user.id
+  });
+
+  res.status(201).json({
+    subscribed: subscribeToPublisher,
+    status: 'success'
+  });
+});
+
+exports.Unsubscribe = catchAsync(async (req, res, next) => {
+  if (!res.locals.subscriber) {
+    return next(
+      new AppError('This user is not subscribed to this publisher.', 404)
+    );
+  }
+
+  await Subscriber.deleteOne({
+    publisher: res.locals.subscriber.publisher,
+    subscriber: res.locals.subscriber.subscriber
+  });
+
+  res.status(204).json({
     status: 'success'
   });
 });
--- routes/subscribeRoutes.js
+++ routes/subscribeRoutes.js
@@ -2,8 +2,25 @@ const express = require('express');
 
 const router = express.Router();
 
+const authController = require('../controllers/authController');
 const subscriberController = require('../controllers/subscriberController');
 
-router.route('/').get(subscriberController.Subscribe);
+router.use(authController.protect);
+router
+  .route('/add')
+  .get(
+    subscriberController.checkSubscription(false),
+    subscriberController.Subscribe
+  );
+router
+  .route('/remove')
+  .get(
+    subscriberController.checkSubscription(false),
+    subscriberController.Unsubscribe
+  );
+// router.route('/check-subscription').get(subscriberController.checkSubscription);
+router
+  .route('/check-subscription')
+  .get(subscriberController.checkSubscription(true));
 
 module.exports = router;
