2c3db9412a70be76d4037c712f22de9726f16da4
Simone Anthony <simoneanthony@Simones-MacBook-Pro.local>
2021-04-20 14:28:25 -0400

    pg uri added, unneeded env variables removed

--- config/prod.js
+++ config/prod.js
@@ -6,9 +6,5 @@ module.exports = {
   SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
   AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
   AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
-  PGHOST: process.env.PGHOST,
-  PGUSER: process.env.USER,
-  PGDATABASE: process.env.PGDATABASE,
-  PGPASSWORD: process.env.PGPASSWORD,
-  PGPORT: process.env.PGPORT
+  PG_URI: process.env.PG_URI
 };
--- db/index.js
+++ db/index.js
@@ -2,11 +2,7 @@ const { Pool } = require('pg');
 const keys = require('../config/keys');
 
 const pool = new Pool({
-  user: keys.PGUSER,
-  host: keys.PGHOST,
-  database: keys.PGDATABASE,
-  password: keys.PGPASSWORD,
-  port: keys.PGPORT
+  connectionString: keys.PG_URI
 });
 
 module.exports = pool;
