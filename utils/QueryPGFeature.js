bba7728fbffe43f906f6a884fc0e534c8ecbd6f1
Simone Anthony <simoneanthony@Simones-MacBook-Pro.local>
2021-04-29 21:04:14 -0400

    insert method added to QueryPGFeature

--- utils/QueryPGFeature.js
+++ utils/QueryPGFeature.js
@@ -2,7 +2,14 @@
 redundency of db queries to PostgresSQL */
 
 class QueryPGFeature {
+  constructor(queryInstance) {
+    // either pool or client can be passed into the parameter here
+    this.queryInstance = queryInstance;
+  }
 
+  async insert(insertInfo, values) {
+    return await this.queryInstance.query(insertInfo, values);
+  }
 }
 
 module.exports = QueryPGFeature;
