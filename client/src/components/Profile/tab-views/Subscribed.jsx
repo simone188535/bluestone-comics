3c13150eeb094b3c8f06c040c28ede8080875d82
Simone Anthony <simone.anthony1@yahoo.com>
2021-12-09 22:00:10 -0500

    subscription table initialized

--- client/src/components/Profile/tab-views/Subscribed.jsx
+++ client/src/components/Profile/tab-views/Subscribed.jsx
@@ -1,11 +1,45 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { getAllSubscriberedTo } from "../../../services";
 import "../subscription.scss";
 
 const Subscribed = ({ profilePageUser }) => {
+  const [errorMessage, setErrorMessage] = useState("");
+  const [allSubscribedList, setAllSubscribedList] = useState([]);
   const { username, id } = profilePageUser;
 
-  useEffect(() => {}, []);
-  return <div>Subscribed</div>;
+  useEffect(() => {
+    async function getAllSubscriberedToAPI() {
+      try {
+        const subbedToUser = await getAllSubscriberedTo(id);
+        setAllSubscribedList(subbedToUser);
+      } catch (err) {
+        setErrorMessage("An error occurred. Please try again later.");
+      }
+    }
+
+    getAllSubscriberedToAPI();
+  }, [id]);
+
+  useEffect(() => {
+    console.log("all Subscribed to user.", allSubscribedList);
+  }, [allSubscribedList]);
+
+  return (
+    <>
+      <table className="subscription sub-table">
+        <tr className="sub-table-row">
+          <th className="sub-table-head">Profile</th>
+          <th className="sub-table-head">Date subscribed</th>
+          <th className="sub-table-head">Subscriber count</th>
+        </tr>
+        <tr className="sub-table-row">
+          <td className="sub-table-data">Alfreds Futterkiste</td>
+          <td className="sub-table-data">Maria Anders</td>
+          <td className="sub-table-data">Germany</td>
+        </tr>
+      </table>
+    </>
+  );
 };
 
 export default Subscribed;
--- client/src/components/Profile/tab-views/Subscribers.jsx
+++ client/src/components/Profile/tab-views/Subscribers.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { getAllSubscribers } from "../../../services";
 import "../subscription.scss";
 
 const Subscribers = ({ profilePageUser }) => {
-  const { username, id } = profilePageUser;
+//   const { username, id } = profilePageUser;
   return <div>Subscribers</div>;
 };
 
