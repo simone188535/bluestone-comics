74f3c52b7f5c27e3970cde8a05ec7fdcc0ac3f6d
Simone Anthony <simone.anthony1@yahoo.com>
2021-12-30 23:03:30 -0500

    allSubscribers query is working, correct data is now displayed

--- controllers/subscriberController.js
+++ controllers/subscriberController.js
@@ -77,19 +77,38 @@ exports.unsubscribe = catchAsync(async (req, res, next) => {
   });
 });
 
-// all subscribers to a specific user
+// all subscribers to a the current user
 exports.getAllSubscribers = catchAsync(async (req, res, next) => {
   const { page } = req.query;
   const { publisherId } = req.params;
 
   const offset = pageOffset(page);
 
+  /* 
+  This query works by first searching for all of the users subscribed to the current users in the subscribers table (this action occurs in the 2nd subquery).
+  It then takes that list of users ie all_users_subbed and displays their publisher_id, username, user_photo and their subscriber count. 
+  Remember, Subqueries are run before the parent query.
+  ex: https://stackoverflow.com/questions/46988538/sql-use-result-from-one-query-for-another-query
+  */
+  /*
+    P.S.: all_users_subbed.* does not need to display publisherId (meaning all_users_subbed.* can be replaced with all_users_subbed.subscriber_id, all_users_subbed.username 
+    and all_users_subbed.user_photo in the outer most SELECT query), it is the same for them all results
+  */
+
   const allSubscribers = await new QueryPG(pool).find(
-    'subscriber_id, username, user_photo',
-    'subscribers INNER JOIN users ON (users.id = subscribers.publisher_id) WHERE publisher_id = ($1) LIMIT 20 OFFSET ($2)',
+    'all_users_subbed.*, (SELECT COUNT(*) FROM subscribers s2 WHERE s2.publisher_id = all_users_subbed.subscriber_id) AS subscribers_sub_count',
+    '(SELECT publisher_id, subscriber_id, u2.username AS username, u2.user_photo AS user_photo FROM subscribers s INNER JOIN users u ON (u.id = s.publisher_id) INNER JOIN users u2 ON (u2.id = s.subscriber_id) WHERE publisher_id = ($1) LIMIT 20 OFFSET ($2)) AS all_users_subbed',
     [publisherId, offset],
     true
   );
+
+  // ORIGINAL
+  // const allSubscribers = await new QueryPG(pool).find(
+  //   'subscriber_id, username, user_photo',
+  //   'subscribers INNER JOIN users ON (users.id = subscribers.publisher_id) WHERE publisher_id = ($1) LIMIT 20 OFFSET ($2)',
+  //   [publisherId, offset],
+  //   true
+  // );
 
   // There are no subscriber to this user yet
   if (!allSubscribers) {
@@ -110,15 +129,15 @@ exports.getAllSubscribedTo = catchAsync(async (req, res, next) => {
   const offset = pageOffset(page);
 
   /* 
-  This query works by first searching for all of the users the current user is subscribed to in the subscribers table (the action occurs in the 3rd subquery).
-  It then takes the list of users (that the current user is subscribed to) and displays their publisher_id, username, user_photo and their subscriber count. 
+  This query works by first searching for all of the users the current user is subscribed to in the subscribers table (this action occurs in the 2nd subquery).
+  It then takes that list of users ie all_users_subbed_to (that the current user is subscribed to) and displays their publisher_id, username, user_photo and their subscriber count. 
+  Remember, Subqueries are run before the parent query.
   ex: https://stackoverflow.com/questions/46988538/sql-use-result-from-one-query-for-another-query
   */
-  // (SELECT COUNT(all_users_subbed_to.subscriber_id) FROM subscribers s2 WHERE s2.subscriber_id = all_users_subbed_to.publisher_id) AS subscribers_sub_count
-  // GROUP BY s.subscriber_id
-  // (SELECT COUNT(*) FROM subscribers s2 WHERE s2.subscriber_id = all_users_subbed_to.publisher_id) AS subscribers_sub_count
-  // Count group by: https://www.w3resource.com/sql/aggregate-functions/count-with-group-by.php
-  // (SELECT COUNT(all_users_subbed_to.subscriber_id) FROM subscribers s2 WHERE s2.subscriber_id = all_users_subbed_to.publisher_id GROUP BY all_users_subbed_to.subscriber_id) AS subscribers_sub_count
+  /* 
+    P.S.: all_users_subbed_to.* does not need to display subscriberId (meaning all_users_subbed_to.* can be replaced with all_users_subbed_to.subscriber_id, all_users_subbed_to.username 
+    and all_users_subbed_to.user_photo in the outer most SELECT query), it is the same for them all
+    */
 
   const allSubscribedTo = await new QueryPG(pool).find(
     'all_users_subbed_to.*, (SELECT COUNT(*) FROM subscribers s2 WHERE s2.publisher_id = all_users_subbed_to.publisher_id) AS subscribers_sub_count',
@@ -131,12 +150,6 @@ exports.getAllSubscribedTo = catchAsync(async (req, res, next) => {
   //   'publisher_id, username, user_photo',
   //   'subscribers INNER JOIN users ON (users.id = subscribers.subscriber_id) WHERE subscriber_id = ($1) LIMIT 20 OFFSET ($2)',
   //   [subscriberId, offset],
-  //   true
-  // );
-  // const allSubscribedTo = await new QueryPG(pool).find(
-  //   '*',
-  //   'subscribers WHERE subscribers.publisher_id = 80',
-  //   [],
   //   true
   // );
 
