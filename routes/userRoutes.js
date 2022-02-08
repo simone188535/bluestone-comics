acaa815ad8e81c9bbea7cb6da17de643c886b3b2
Simone Anthony <simone.anthony1@yahoo.com>
2022-02-07 21:43:31 -0500

    protect route moved in user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,22 +6,18 @@ const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
 
 // define the home page route
-router.route('/').get(authController.protect, userController.getAllUsers);
+router.route('/get-user').get(userController.getUser);
 router.route('/signup').post(authController.signup);
 router.route('/login').post(authController.login);
 router.route('/forgot-password').post(authController.forgotPassword);
 router.route('/reset-password/:resetToken').patch(authController.resetPassword);
-router
-  .route('/update-password')
-  .patch(authController.protect, authController.updatePassword);
-router
-  .route('/update-me')
-  .patch(authController.protect, userController.updateMe);
-router
-  .route('/delete-me')
-  .delete(authController.protect, userController.deleteMe);
 
-router.route('/get-me').get(authController.protect, userController.getMe);
-router.route('/get-user').get(userController.getUser);
+router.use(authController.protect);
+router.route('/').get(userController.getAllUsers);
+router.route('/update-password').patch(authController.updatePassword);
+router.route('/update-me').patch(userController.updateMe);
+router.route('/delete-me').delete(userController.deleteMe);
+
+router.route('/get-me').get(userController.getMe);
 
 module.exports = router;
