3b5b1eefabdbf7fd9cb268340353edadb0ad7abe
Simone Anthony <simone.anthony1@yahoo.com>
2022-02-21 22:15:21 -0500

    Formik Submission Status component created

---
+++ client/src/components/CommonUI/FormikSubmissionStatus/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useFormikContext } from "formik";
+
+const FormikStatus = ({ err, successMessage, errMsg }) => {
+  const { submitCount, isSubmitting, isValid } = useFormikContext();
+  if (submitCount > 0 && !isSubmitting && isValid) {
+    if (err) {
+      return (
+        <div className="text-danger text-center">
+          {errMsg || "Something went wrong. Please try again later."}
+        </div>
+      );
+    }
+    return <div className="text-blue text-center">{successMessage}</div>;
+  }
+  return "";
+};
+
+export default FormikStatus;
