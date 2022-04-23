01669101a5e65ae7fb2091c0dd1a7fc2c113041a
Simone Anthony <simone.anthony1@yahoo.com>
2022-04-23 00:47:01 -0400

    urlSlug query param corrected

--- routes/readRoutes.js
+++ routes/readRoutes.js
@@ -5,9 +5,9 @@ const router = express.Router();
 const readController = require('../controllers/readController');
 // THESE NEED TO BE TESTED !!!!!!!!!!!
 router.route('/').get(readController.getAllBooks);
-router.route('/:url-slug/book/:bookId').get(readController.getBook);
+router.route('/:urlSlug/book/:bookId').get(readController.getBook);
 router
-  .route('/:url-slug/book/:bookId/Issue/:issueNumber')
+  .route('/:urlSlug/book/:bookId/Issue/:issueNumber')
   .get(readController.getIssue);
 
 module.exports = router;
