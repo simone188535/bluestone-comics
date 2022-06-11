ccac1cd2dc1ebe423d5c37520b7cd5b068eb2586
Simone Anthony <simone.anthony1@yahoo.com>
2022-06-10 21:34:16 -0400

    Bookmarrk services api calls defined

--- client/src/services/Bookmark.services.js
+++ client/src/services/Bookmark.services.js
@@ -1,72 +1,27 @@
 import axios from "axios";
-import { configObjects, configPageNumber } from "../utils/configObjects";
+import { configObjects } from "../utils/configObjects";
 
-function createBook(formData, config = {}) {
-  // send request to create a book which will contain the first issue
-  const jwtToken = localStorage.getItem("jwtToken");
-
-  config.headers = {
-    Authorization: `Bearer ${jwtToken}`,
-  };
-
-  return axios.post("/api/v1/publish", formData, config);
-}
-
-function getBookAndIssueImagePrefix(
-  bookId = "",
-  issueNumber = "",
-  config = {}
-) {
-  // send request to create a book which will contain the first issue
-  const jwtToken = localStorage.getItem("jwtToken");
-
-  config.headers = {
-    Authorization: `Bearer ${jwtToken}`,
-  };
-
-  let optionalBookIssue = ``;
-
-  if (bookId) {
-    optionalBookIssue += `/${bookId}`;
-    if (issueNumber) {
-      optionalBookIssue += `/${issueNumber}`;
-    }
-  }
-
-  return axios.get(
-    `/api/v1/publish/get-book-and-issue-image-prefix${optionalBookIssue}`,
-    config
-  );
-}
-
-function getUsersBook(urlSlug, bookId) {
-  return axios.get(
-    `/api/v1/publish/${urlSlug}/book/${bookId}`,
+function createBookmark(bookId) {
+  return axios.post(
+    `api/v1/bookmark/book/${bookId}/create`,
+    {},
     configObjects()
   );
 }
 
-function getUsersIssue(urlSlug, bookId, issueId) {
-  return axios.get(
-    `/api/v1/publish/${urlSlug}/book/${bookId}/issue/${issueId}`,
+function getBookmark(bookId) {
+  return axios.get(`/api/v1/bookmark/book/${bookId}`, configObjects());
+}
+
+function getAllBookmarks(subscriberId) {
+  return axios.get(`/api/v1/bookmark/get-all/subscriber/${subscriberId}`);
+}
+
+function deleteBookmark(bookId) {
+  return axios.delete(
+    `/api/v1/bookmark/book/${bookId}/delete`,
     configObjects()
   );
 }
 
-function getUsersIssues(urlSlug, bookId, pageNumber) {
-  const config = configObjects();
-  const appendedConfig = configPageNumber(config, pageNumber);
-
-  return axios.get(
-    `/api/v1/publish/${urlSlug}/book/${bookId}/issues`,
-    appendedConfig()
-  );
-}
-
-export {
-  createBook,
-  getBookAndIssueImagePrefix,
-  getUsersBook,
-  getUsersIssue,
-  getUsersIssues,
-};
+export { createBookmark, getBookmark, getAllBookmarks, deleteBookmark };
