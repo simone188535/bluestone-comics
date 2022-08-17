8c696adf1f662fc013d1c886f1fbd5a0ee39167d
Simone Anthony <simone.anthony1@yahoo.com>
2022-08-16 20:54:57 -0400

    bookupload and issue upload templates created, default props added

---
+++ client/src/components/Upload/BookUpload.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import slugify from "slugify";
+import { Field, ErrorMessage, useField, useFormikContext } from "formik";
+
+import FileInputSingleUpload from "../CommonUI/FileInputSingleUpload";
+import IMAGE_UPLOAD_DIMENSIONS from "../../utils/Constants";
+
+const { WIDTH, HEIGHT } = IMAGE_UPLOAD_DIMENSIONS.THUMBNAIL;
+
+const UrlSlugifedField = (props) => {
+  /*
+        The default value of this field is dependent on the value of the book title field. The user
+        is still able to customize it though.
+    */
+  const {
+    values: { bookTitle },
+    setFieldValue,
+  } = useFormikContext();
+  const [field] = useField(props);
+
+  React.useEffect(() => {
+    setFieldValue(props.name, slugify(bookTitle));
+    // eslint-disable-next-line react/destructuring-assignment
+  }, [bookTitle, setFieldValue, props.name]);
+
+  return (
+    <>
+      <input {...props} {...field} />
+    </>
+  );
+};
+
+const BookUpload = ({
+  bookCoverPhotoPrevExistingData = {
+    toBeRemovedField: null,
+    hasPrevUploadedData: false,
+  },
+}) => {
+  const { values } = useFormikContext();
+  return (
+    <>
+      <Field
+        className="form-input form-item"
+        name="bookTitle"
+        type="text"
+        placeholder="Book Title"
+      />
+      <ErrorMessage
+        className="error-message error-text-color"
+        component="div"
+        name="bookTitle"
+      />
+      <FileInputSingleUpload
+        identifier="bookCoverPhoto"
+        triggerText="Select Book Thumbnail Photo"
+        toBeRemovedField={bookCoverPhotoPrevExistingData.toBeRemovedField}
+        hasPrevUploadedData={bookCoverPhotoPrevExistingData.hasPrevUploadedData}
+      />
+      <ErrorMessage
+        className="error-message error-text-color"
+        component="div"
+        name="bookCoverPhoto"
+      />
+      <div className="form-header-subtext">
+        Thumbnail size must be: <strong>{`${WIDTH} x ${HEIGHT}`}</strong>
+      </div>
+      <Field
+        className="form-input form-textarea"
+        name="bookDescription"
+        as="textarea"
+        placeholder="Book Description"
+        autoComplete="on"
+      />
+      <ErrorMessage
+        className="error-message error-text-color"
+        component="div"
+        name="bookDescription"
+      />
+      <UrlSlugifedField
+        className="form-input form-item slug-field"
+        name="urlSlug"
+        type="text"
+        placeholder="URL Slug"
+        autoComplete="on"
+      />
+      <div className="form-header-subtext">
+        <strong>Your Comic URL will be similar to this: </strong>{" "}
+        https://bluestonecomics.com/api/v1/read/
+        <strong>{values.urlSlug ? values.urlSlug : "<URL Slug>"}</strong>
+        /book/1234
+      </div>
+      <ErrorMessage
+        className="error-message error-text-color"
+        component="div"
+        name="urlSlug"
+      />
+    </>
+  );
+};
+export default BookUpload;
---
+++ client/src/components/Upload/IssueUpload.jsx
@@ -0,0 +1,132 @@
+import React from "react";
+import { Field, ErrorMessage } from "formik";
+
+import Checkboxes from "../CommonUI/Checkboxes";
+import FileInputSingleUpload from "../CommonUI/FileInputSingleUpload";
+import FileInputMultipleUpload from "../CommonUI/FileInputMultipleUpload";
+import WorkCredits from "./WorkCredits";
+import IMAGE_UPLOAD_DIMENSIONS from "../../utils/Constants";
+
+const { WIDTH, HEIGHT } = IMAGE_UPLOAD_DIMENSIONS.THUMBNAIL;
+const IssueUpload = ({
+  issueCoverPhotoPrevExistingData = {
+    toBeRemovedField: null,
+    hasPrevUploadedData: false,
+  },
+  multiFileUploadPrevExistingData = {
+    toBeRemovedField: null,
+    hasPrevUploadedData: false,
+  },
+}) => {
+  return (
+    <>
+      <Field
+        className="form-input form-item"
+        name="issueTitle"
+        type="text"
+        placeholder="Issue Title"
+        autoComplete="on"
+      />
+      <ErrorMessage
+        className="error-message error-text-color"
+        component="div"
+        name="issueTitle"
+      />
+      <FileInputSingleUpload
+        identifier="issueCoverPhoto"
+        triggerText="Select Issue Thumbnail Photo"
+        toBeRemovedField={issueCoverPhotoPrevExistingData.toBeRemovedField}
+        hasPrevUploadedData={
+          issueCoverPhotoPrevExistingData.hasPrevUploadedData
+        }
+      />
+      <ErrorMessage
+        className="error-message error-text-color"
+        component="div"
+        name="issueCoverPhoto"
+      />
+      <div className="form-header-subtext">
+        Thumbnail size must be: <strong>{`${WIDTH} x ${HEIGHT}`}</strong>
+      </div>
+      <Field
+        className="form-input form-textarea"
+        name="issueDescription"
+        as="textarea"
+        placeholder="Issue Description"
+        autoComplete="on"
+      />
+      <ErrorMessage
+        className="error-message error-text-color"
+        component="div"
+        name="issueDescription"
+      />
+      <div className="form-header-text">
+        Select the applicable <strong>genres</strong>
+      </div>
+      <ul className="checkbox-group upload-checkboxes">
+        <Checkboxes
+          identifier="genres"
+          type="multiple"
+          wrapperElement="li"
+          checkboxValue={[
+            { name: "Action/Adventure" },
+            { name: "Anthropomorphic" },
+            { name: "Children" },
+            { name: "Comedy" },
+            { name: "Crime" },
+            { name: "Drama" },
+            { name: "Family" },
+            { name: "Fantasy" },
+            { name: "Graphic Novels" },
+            { name: "Historical" },
+            { name: "Horror" },
+            { name: "LGBTQ" },
+            { name: "Mature" },
+            { name: "Music" },
+            { name: "Mystery" },
+            { name: "Mythology" },
+            { name: "Psychological" },
+            { name: "Romance" },
+            { name: "School Life" },
+            { name: "Sci-Fi" },
+            { name: "Slice of Life" },
+            { name: "Sport" },
+            { name: "Superhero" },
+            { name: "Supernatural" },
+            { name: "Thriller" },
+            { name: "War" },
+            { name: "Western" },
+            { name: "Zombies" },
+          ]}
+        />
+      </ul>
+      <ErrorMessage
+        className="error-message error-text-color"
+        component="div"
+        name="genres"
+      />
+      <FileInputMultipleUpload
+        identifier="issueAssets"
+        dropzoneInnerText={`<div class="form-header-text"> Drag 'n' drop <strong>Issue Pages</strong> here, or click to select files.<br/>(Recommended file size: ${IMAGE_UPLOAD_DIMENSIONS.STANDARD_UPLOAD_SIZE.WIDTH} x ${IMAGE_UPLOAD_DIMENSIONS.STANDARD_UPLOAD_SIZE.HEIGHT})<div>`}
+        toBeRemovedField={multiFileUploadPrevExistingData.toBeRemovedField}
+        hasPrevUploadedData={
+          multiFileUploadPrevExistingData.hasPrevUploadedData
+        }
+      />
+      <div className="form-header-text">
+        Give <strong>Credits</strong> to yourself and any other existing users
+        by selecting the role(s) they fulfilled while helping to create this
+        Book/Issue:
+      </div>
+      <WorkCredits identifier="workCredits" />
+      <div className="form-header-subtext">
+        <strong>
+          *Tip: There is no need to select every available field if you are the
+          only creator. Selecting writer and artist will suffice. It may
+          overcomplicate the search for any of your other works.
+        </strong>
+      </div>
+    </>
+  );
+};
+export default IssueUpload;
