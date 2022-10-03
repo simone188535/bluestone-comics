6938bd1f61f3b4d41eee6fcb377643b98221195c
Simone Anthony <simone.anthony1@yahoo.com>
2022-10-03 11:53:00 -0400

    unused hook removed

--- client/src/components/Details/DisplayIssues.jsx
+++ client/src/components/Details/DisplayIssues.jsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import moment from "moment";
 import { getIssues, deleteIssue } from "../../services";
 import DeleteWorkModal from "../Upload/DeleteWorkModal";
-import useIsLatestIssue from "../../hooks/useIsLatestIssue";
 
 const MappedIssue = ({
   title,
