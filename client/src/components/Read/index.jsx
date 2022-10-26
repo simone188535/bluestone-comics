77774ffe45a88ef5f713d7646a67193910ffddb2
Simone Anthony <simone.anthony1@yahoo.com>
2022-10-25 21:50:02 -0400

    sticky footer inital styles added, logic for issuePagination state added and is working

--- client/src/components/Read/index.jsx
+++ client/src/components/Read/index.jsx
@@ -1,15 +1,107 @@
 import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getIssue, getIssues } from "../../services";
+import ErrMsg from "../CommonUI/ErrorMessage";
 import "./read.scss";
+
+const IssuePagination = ({ setDeleteErr }) => {
+  const { urlSlug, bookId, issueNumber } = useParams();
+  const [issuePagination, setIssuePagination] = useState({
+    firstIssueNum: 1,
+    prevIssueNum: null,
+    nextIssueNum: null,
+    lastIssueNum: null,
+  });
+
+  // get the total number of issues and use it to set state in lastIssueNum
+  useEffect(() => {
+    try {
+      (async () => {
+        const {
+          data: { issueTotal },
+        } = await getIssues(urlSlug, bookId, issueNumber);
+
+        setIssuePagination((prevState) => ({
+          ...prevState,
+          lastIssueNum: issueTotal,
+        }));
+      })();
+    } catch (err) {
+      setDeleteErr(true);
+    }
+  }, [bookId, issueNumber, setDeleteErr, urlSlug]);
+
+  // if a issueNumber is present use it to set state in prevIssueNum, nextIssueNum
+  useEffect(() => {
+    if (issueNumber) {
+      const issueNum = Number(issueNumber);
+      setIssuePagination((prevState) => ({
+        ...prevState,
+        /* prevIssueNum should never be less than 1 (because there are no issues  
+        with issue numbers lower than 1), if the value is less than 1, prevIssueNum 
+        should be null
+        */
+        prevIssueNum: issueNum - 1 > 0 ? issueNum - 1 : null,
+        /*
+          nextIssueNum should never be greater than lastIssueNum, if it is
+          lastIssueNum should be null
+        */
+        nextIssueNum:
+          issueNum + 1 <= issuePagination.lastIssueNum ? issueNum + 1 : null,
+      }));
+    }
+  }, [issueNumber, issuePagination.lastIssueNum]);
+
+  useEffect(() => {
+    console.log(issuePagination);
+  }, [issuePagination]);
+
+  return <section className="read-nav-sticky-footer">??</section>;
+};
 
 const Read = () => {
   const [pages, setPages] = useState([]);
-  useEffect(() => {}, []);
+  const [deleteErr, setDeleteErr] = useState(false);
+  const { urlSlug, bookId, issueNumber } = useParams();
+  useEffect(() => {
+    try {
+      (async () => {
+        const {
+          data: { issueAssets },
+        } = await getIssue(urlSlug, bookId, issueNumber);
+        setPages(issueAssets);
+      })();
+    } catch (err) {
+      setDeleteErr(true);
+    }
+  }, [bookId, issueNumber, urlSlug]);
   return (
-    <div className="container-fluid read-page">
+    <main className="container-fluid read-page min-vh100">
       <div className="row">
-        <h2>Read</h2>
+        {/* <h2>Read</h2> */}
+        {!deleteErr ? (
+          <ul className="issue-page-list-container">
+            {pages.map(({ id, page_number: pageNum, photo_url: photoUrl }) => (
+              <li className="issue-page" key={`issue-page-${id}`}>
+                <img
+                  src={photoUrl}
+                  className="issue-page-img"
+                  alt={`issue-page-${pageNum}`}
+                />
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p className="read-err-msg text-center">
+            <ErrMsg
+              errorStatus={deleteErr}
+              messageText="An Error occurred. Please try again later."
+            />
+          </p>
+        )}
       </div>
-    </div>
+      <IssuePagination setDeleteErr={setDeleteErr} />
+    </main>
   );
 };
 export default Read;
