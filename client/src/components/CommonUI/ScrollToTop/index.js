498ab268e19dd7f39250bfbdbcf33ab83616af2c
Simone Anthony <simone.anthony1@yahoo.com>
2022-12-23 23:56:25 -0500

    ScrollToTop

--- client/src/components/CommonUI/ScrollToTop/index.js
+++ client/src/components/CommonUI/ScrollToTop/index.js
@@ -1,14 +1,14 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-import scrollToTop from "../../../utils/scrollToTop";
+import { scrollToTop, defaultScrollToTopObj } from "../../../utils/scrollToTop";
 
 /*
     This resets the scroll bar to the top of the page for a given component when 
     the url changes. Inspired by: https://stackoverflow.com/questions/58431946/why-does-my-react-router-link-bring-me-to-the-middle-of-a-page
     https://v5.reactrouter.com/web/guides/scroll-restoration/scroll-to-top
 */
-function ScrollToTop({ coords }) {
+function ScrollToTop({ coords = defaultScrollToTopObj }) {
   const { pathname } = useLocation();
 
   useEffect(() => {
