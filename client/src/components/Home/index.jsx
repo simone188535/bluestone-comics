626a2d3a3607945cc0c28d01d22f0af90cea49a6
Simone Anthony <simone.anthony1@yahoo.com>
2023-02-06 22:04:26 -0500

    mobile designs for home page complete

--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -1,20 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import wolverine from "../../assets/wolverine.jpeg";
+// import starlight from "../../assets/starlight.jpeg";
+// import supergirl from "../../assets/starlight.jpeg"
 import "./home.scss";
 
 const Home = () => {
   return (
     <main className="container-fluid home-page min-vh100">
-      <section className="one-third-display sect-one">
-        <section className="panel">
-          <h1>Bluestone Comics...</h1>{" "}
-          <h2>
-            is a celebration of american comics books. Our goal is to promote
-            their creation and culture.
-          </h2>
-        </section>
-        <section className="panel" />
+      <section className="hero-container sect-one">
+        <div className="hero-image-container">
+          <img className="hero-image" src={wolverine} alt="wolverine" />
+        </div>
+        <article className="desc">
+          <h1 className="primary-header">Welcome to Bluestone Comics</h1>
+          <p className="details">
+            Bluestone comics is a celebration of american comics books. Our goal
+            is to promote their creation and culture.
+          </p>
+          <Link to="/upload" className="link-button">
+            <button
+              type="button"
+              className="bsc-button transparent transparent-black "
+            >
+              Upload Your Work
+            </button>
+          </Link>
+        </article>
       </section>
-      <section className="one-third-display sect-two">
+      {/* <section className="one-third-display sect-two">
         <section className="panel" />
         <section className="panel">
           <h1>Bluestone Comics...</h1>{" "}
@@ -33,7 +47,7 @@ const Home = () => {
           </h2>
         </section>
         <section className="panel" />
-      </section>
+      </section> */}
     </main>
   );
 };
