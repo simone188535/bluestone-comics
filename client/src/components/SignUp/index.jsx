ac2cbbe831bfa2ab6679eb930c3e7b1d5ae3addd
Simone Anthony <simone.anthony1@yahoo.com>
2023-02-13 21:32:26 -0500

    signup eslint resolved, min-vh100 added

---
+++ client/src/components/SignUp/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignUpForm from "./form/SignUpForm";
+import "./sign-up.scss";
+
+const SignUp = () => {
+  return (
+    <div className="sign-up-page min-vh100">
+      <SignUpForm />
+    </div>
+  );
+};
+export default SignUp;
