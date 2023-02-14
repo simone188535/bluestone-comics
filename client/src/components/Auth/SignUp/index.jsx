77b2e89f038a3ba25c9927fe6c1c0ff01e19a162
Simone Anthony <simone.anthony1@yahoo.com>
2023-02-13 21:38:44 -0500

    min-vh100 added to sign up

--- client/src/components/Auth/SignUp/index.jsx
+++ client/src/components/Auth/SignUp/index.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import SignUpForm from './form/SignUpForm';
-import '../auth.scss';
-import './sign-up.scss';
+import React from "react";
+import SignUpForm from "./form/SignUpForm";
+import "../auth.scss";
+import "./sign-up.scss";
 
 const SignUp = () => {
   return (
-    <div className="sign-up-page auth-pages container">
+    <div className="sign-up-page auth-pages container min-vh100">
       <SignUpForm />
     </div>
   );
