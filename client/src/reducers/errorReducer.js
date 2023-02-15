98cff60efd6e0f6fe4a8d940e1cbbcc5a84bb391
Simone Anthony <simone.anthony1@yahoo.com>
2023-02-14 23:30:40 -0500

    eslint corrections in errorReducer, serviceWorker

--- client/src/reducers/errorReducer.js
+++ client/src/reducers/errorReducer.js
@@ -1,20 +1,17 @@
-
-import { SET_ERROR, REMOVE_ERROR } from '../actions/types';
+import { SET_ERROR, REMOVE_ERROR } from "../actions/types";
 
 const initialState = {
-    hasError: false,
-    errorMessage: null
+  hasError: false,
+  errorMessage: null,
+};
+// eslint-disable-next-line default-param-last
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case SET_ERROR:
+      return { ...state, hasError: true, errorMessage: action.error };
+    case REMOVE_ERROR:
+      return initialState;
+    default:
+      return state;
   }
-export default function(state = initialState, action) {
-    switch (action.type) {
-        case SET_ERROR: 
-            return Object.assign({}, state, {
-                hasError: true,
-                errorMessage: action.error
-            });
-        case REMOVE_ERROR: 
-            return initialState
-         default: 
-            return state;
-        }
-    }
+}
