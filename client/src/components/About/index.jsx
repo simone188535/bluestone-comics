cecb06a64f2f9113db3044633d9b8cc2bac670ee
Simone Anthony <simone.anthony1@yahoo.com>
2023-02-15 23:59:43 -0500

    about page complete

--- client/src/components/About/index.jsx
+++ client/src/components/About/index.jsx
@@ -3,10 +3,17 @@ import "./about.scss";
 
 const About = () => {
   return (
-    <div className="container-fluid about-page">
-      <div className="row">
-        <h2>About</h2>
-      </div>
+    <div className="container about-page min-vh100">
+      <h2 className="header">About</h2>
+      <p className="desc">
+        Bluestone comics is a celebration of american comics books. Our goal is
+        to promote their creation and culture by allowing our users to upload
+        their own work.
+      </p>
+      <p className="desc">
+        In the future, our hope is to sponsor and reward comics that stand out
+        from the competition.
+      </p>
     </div>
   );
 };
