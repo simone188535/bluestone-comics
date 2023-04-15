68fbbff5a97fc9cddc1dda488f77b8a8d119c0be
Simone Anthony <simone.anthony1@yahoo.com>
2023-04-15 01:37:34 -0400

    seo added to page not found

--- client/src/components/PageNotFound/index.jsx
+++ client/src/components/PageNotFound/index.jsx
@@ -1,21 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import MetaTags from "../MetaTags";
 import "./page-not-found.scss";
 
 const PageNotFound = () => {
   return (
-    <div className="container not-found-page min-vh100">
-      <h2 className="header">Page Not Found!</h2>
-      <p className="desc">
-        Looks like you entered a broken link or entered a URL that doesn&#39;t
-        exist on this site.
-      </p>
-      <p className="desc">
-        <Link to="/" className="back-to-home-link">
-          Back to our site
-        </Link>
-      </p>
-    </div>
+    <>
+      <MetaTags
+        title="Bluestone Comics | Page Not Found"
+        description="The page you are looking for was not found!"
+      >
+        <meta name="robots" content="noindex, nofollow" />
+      </MetaTags>
+      <div className="container not-found-page min-vh100">
+        <h1 className="header">404: Uh-Oh! Page Not Found!</h1>
+        <p className="desc">
+          Looks like you entered a broken link or entered a URL that doesn&#39;t
+          exist on this site.
+        </p>
+        <p className="desc">
+          <Link to="/" className="back-to-home-link">
+            Back to our site
+          </Link>
+        </p>
+      </div>
+    </>
   );
 };
 export default PageNotFound;
