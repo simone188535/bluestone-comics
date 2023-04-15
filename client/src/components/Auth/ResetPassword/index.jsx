21e771c58593c8025371467a7e3a4476713d1367
Simone Anthony <simone.anthony1@yahoo.com>
2023-04-14 20:52:35 -0400

    ResetPassword metadata added

--- client/src/components/Auth/ResetPassword/index.jsx
+++ client/src/components/Auth/ResetPassword/index.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import ResetPasswordForm from "./form/ResetPasswordForm";
+import MetaTags from "../../MetaTags";
 import "../auth.scss";
 import "./reset-password.scss";
 
 const ResetPassword = () => {
   return (
-    <div className="container auth-pages reset-password-page min-vh100">
-      <ResetPasswordForm />
-    </div>
+    <>
+      <MetaTags
+        title="Bluestone Comics | Reset Password"
+        description="Reset your password using the link from your email."
+      >
+        <meta name="robots" content="noindex, nofollow" />
+      </MetaTags>
+      <div className="container auth-pages reset-password-page min-vh100">
+        <ResetPasswordForm />
+      </div>
+    </>
   );
 };
 export default ResetPassword;
