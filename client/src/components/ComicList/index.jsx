a32baa94bb0a68d7fa7ad3f26bb014110761f735
Simone Anthony <simone.anthony1@yahoo.com>
2023-04-27 21:24:04 -0400

    Comic List removed from robots.txt and given no index

--- client/src/components/ComicList/index.jsx
+++ client/src/components/ComicList/index.jsx
@@ -1,13 +1,19 @@
 import React from "react";
+import MetaTags from "../MetaTags";
 import "./comic-list.scss";
 
 const ComicList = () => {
   return (
-    <div className="container-fluid comic-list-page min-vh100">
-      <div className="row">
-        <h2>Comic List</h2>
+    <>
+      <MetaTags title="Bluestone Comics | Comic List" description="">
+        <meta name="robots" content="noindex, nofollow" />
+      </MetaTags>
+      <div className="container-fluid comic-list-page min-vh100">
+        <div className="row">
+          <h2>Comic List</h2>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 export default ComicList;
