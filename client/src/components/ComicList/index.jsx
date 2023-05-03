5525a0f1962bf3aeade3e5c2cca13019bc29dca3
Simone Anthony <simone.anthony1@yahoo.com>
2023-05-03 19:57:17 -0400

    Comic list redirect removed

--- client/src/components/ComicList/index.jsx
+++ client/src/components/ComicList/index.jsx
@@ -1,4 +1,4 @@
-import React, { Redirect } from "react-router-dom";
+import React from "react-router-dom";
 import MetaTags from "../MetaTags";
 import "./comic-list.scss";
 
@@ -10,7 +10,6 @@ const ComicList = () => {
         canonical="https://www.bluestonecomics.com/comic-list"
         description="Find and Read Your Favorite American Comics. There is tons of new work to explore!"
       />
-      <Redirect to="/page-not-found" />
       <div className="container-fluid comic-list-page min-vh100">
         <div className="row">
           <h2>Comic List</h2>
