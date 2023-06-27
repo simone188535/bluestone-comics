f1ca7cab1149f80ee89d098678c68f663ee43798
Simone Anthony <simone.anthony1@yahoo.com>
2023-06-27 15:47:52 -0400

    ArticleItems rendering correctly

--- client/src/components/Articles/index.jsx
+++ client/src/components/Articles/index.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import ArticleItems from "../CommonUI/ArticleItems";
+import batmanLarge from "../../assets/homepage/batman-welcome-Large.jpg";
 import "./articles.scss";
 
 const Articles = () => {
   return (
     <div className="container-fluid articles-page min-vh100">
       <main className="articles-container">
-        <ArticleItems articleList={[]} />
+        <ArticleItems
+          articleList={[
+            {
+              id: 1,
+              header: "Header",
+              desc: "description",
+              date: new Date().toISOString(),
+              imgSrc: batmanLarge,
+              alt: "placeholder",
+            },
+          ]}
+        />
       </main>
     </div>
   );
--- client/src/components/CommonUI/ArticleItems/index.jsx
+++ client/src/components/CommonUI/ArticleItems/index.jsx
@@ -2,20 +2,29 @@ import React from "react";
 import "./article-items.scss";
 
 const ArticleItems = ({ articleList }) => {
-  return articleList?.map(({ header, desc, date, imgSrc, alt }) => (
-    <article className="article-items">
-      <section className="article-body">
-        <h2 className="header">{header}</h2>
-        {desc && <section className="desc">{desc}</section>}
-        {date && <div className="date">{date}</div>}
-      </section>
-      {imgSrc && alt && (
-        <figure>
-          <img src={imgSrc} alt={alt} />
-        </figure>
-      )}
-    </article>
-  ));
+  const articles =
+    articleList?.length > 0
+      ? articleList.map((item) => {
+          const { key, header, desc, date, imgSrc, alt } = item;
+
+          return (
+            <article className="article-items" key={`article-items-${key}`}>
+              <section className="article-body">
+                <h2 className="header">{header}</h2>
+                {desc && <section className="desc">{desc}</section>}
+                {date && <div className="date">{date}</div>}
+              </section>
+              {imgSrc && alt && (
+                <figure>
+                  <img src={imgSrc} alt={alt} />
+                </figure>
+              )}
+            </article>
+          );
+        })
+      : null;
+
+  return articles;
 };
 
 export default ArticleItems;
