b09f279005825db9cf8b73fc49507d265c4be653
Simone Anthony <simone.anthony1@yahoo.com>
2023-06-27 14:46:01 -0400

    articles container added to Articles

--- client/src/components/Articles/index.jsx
+++ client/src/components/Articles/index.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import ArticleItem from "../CommonUI/ArticleItem";
 import "./articles.scss";
 
 const Articles = () => {
   return (
-    <div className="container-fluid articles-page">
-      <div className="row">
-        <h2>Articles</h2>
-      </div>
+    <div className="container-fluid articles-page min-vh100">
+      <main className="articles-container">
+        <ArticleItem articleList={[]} />
+      </main>
     </div>
   );
 };
