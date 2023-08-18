196b7f30da040daa630bdf5040501ff31d252f3c
Simone Anthony <simone.anthony1@yahoo.com>
2023-08-17 21:35:59 -0400

    upload-work-tutorial stylesheet added

--- client/src/components/Articles/pages/UploadWorkTutorial/index.jsx
+++ client/src/components/Articles/pages/UploadWorkTutorial/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import moment from "moment";
 import MetaTags from "../../../MetaTags";
+import "./upload-work-tutorial.scss";
 
 function UploadWorkTutorial({
   // img,
