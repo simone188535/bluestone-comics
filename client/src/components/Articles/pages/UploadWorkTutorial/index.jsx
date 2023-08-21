f3ba43f16d4d285cb4819dd9028a365ab950d126
Simone Anthony <simone.anthony1@yahoo.com>
2023-08-21 16:39:06 -0400

    most bolds removed for visual appeal

--- client/src/components/Articles/pages/UploadWorkTutorial/index.jsx
+++ client/src/components/Articles/pages/UploadWorkTutorial/index.jsx
@@ -57,6 +57,8 @@ function UploadWorkTutorial({
           <strong className="mid-bold">Self-publishing</strong> gives you full
           creative control over your comic book. You have the freedom to create
           the story, artwork, and design exactly as you envision it.{" "}
+        </p>
+        <p className="article-para">
           <strong className="mid-bold">
             Bluestone Comics allows you to publish your comic books online
           </strong>{" "}
@@ -91,53 +93,50 @@ function UploadWorkTutorial({
               to create a comic book and its first issue.
             </li>
             <li className="bold-marker">
-              <div className="inline-li">
-                <strong className="mid-bold">Add a book title.</strong> The name
-                chosen for the book will also serve as the name of the work in
-                the URL.{" "}
-              </div>
-              <div className="mt-sm">
-                <strong className="mid-bold">For example, </strong> If the
-                chosen book title is{" "}
-                <strong className="mid-bold">&#34;my first comic&#34;</strong>.
-                The URL for the comic book would be similar to:{" "}
-              </div>
-              <div className="mt-sm">
-                <strong className="mid-bold example-link">
+              <p className="mb-sm">
+                Add a book title. The name chosen for the book will also serve
+                as the name of the work in the URL.{" "}
+              </p>
+              <p className="mb-sm">
+                For example, If the chosen book title is &#34;my first
+                comic&#34;. The URL for the comic book would be similar to:{" "}
+              </p>
+              <p>
+                <strong className="example-link">
                   https://bluestonecomics.com/read/my-first-comic/book/example/issue/1
                 </strong>
-              </div>
+              </p>
             </li>
             <li className="bold-marker">
-              <strong className="mid-bold">
-                Select a book thumbnail photo.
-              </strong>{" "}
-              The book thumbnail photo is essentially the book&#39;s cover
-              photo. It is the cover image for the “front of the book”. It{" "}
-              <strong className="mid-bold">must</strong> have a{" "}
-              <strong className="mid-bold">width of 320 pixels</strong> and a{" "}
-              <strong className="mid-bold">height of 485 pixels</strong>. The{" "}
-              <strong className="mid-bold">maximum file size</strong> allowed is{" "}
-              <strong className="mid-bold">1 Megabyte (MB).</strong>
+              <p className="mb-sm">Select a book thumbnail photo.</p>{" "}
+              <p className="mb-sm">
+                The book thumbnail photo is essentially the book&#39;s cover
+                photo. It is the cover image for the &#34;front of the
+                book&#34;.
+              </p>
+              <p>
+                <span className="note">Note: </span>
+                The thumbnail must have a width of 320 pixels and a height of
+                485 pixels. The maximum file size allowed is 1 Megabyte (MB).
+              </p>
+            </li>
+            <li className="bold-marker">Add a book description.</li>
+            <li className="bold-marker">
+              <p className="mb-sm">Update the URL Slug.</p>
+              <p className="mb-sm">
+                <span className="note">Note: </span>If you&#39;d like the URL
+                Slug to be different from the book title. See step 3.
+              </p>
             </li>
             <li className="bold-marker">
-              <strong className="mid-bold">Add a book description.</strong>
+              <p className="mb-sm">Select the content rating for this book.</p>{" "}
+              <p>
+                Make sure to check the creator guidelines to see if your work
+                meets the safety criteria for all users.
+              </p>
             </li>
             <li className="bold-marker">
-              <strong className="mid-bold">Update the URL Slug</strong> (If
-              you&#39;d like it to be different from the book title. See step
-              3).
-            </li>
-            <li className="bold-marker">
-              <strong className="mid-bold">
-                Select the content rating for this book.
-              </strong>{" "}
-              Make sure to check the creator guidelines to see if your work
-              meets the safety criteria for all users.
-            </li>
-            <li className="bold-marker">
-              <strong className="mid-bold">Select the genres</strong> applicable
-              for the book.
+              Select the genres applicable for the book.
             </li>
           </ol>
 
@@ -150,93 +149,110 @@ function UploadWorkTutorial({
 
           <ol className="article-list" start="9">
             <li className="bold-marker">
-              <strong className="mid-bold">Add an issue title</strong> (This
-              will have no effect on the &#34;URL Slug&#34;).
+              <p className="mb-sm">
+                Add an issue title. (This will have no effect on the &#34;URL
+                Slug&#34;.)
+              </p>
             </li>
 
             <li className="bold-marker">
-              <strong className="mid-bold">
-                Select an issue thumbnail photo.
-              </strong>{" "}
-              The issue thumbnail is the cover photo for the issue. if you&#39;d
-              like, you may reuse the same thumbnail as the one used in the book
-              thumbnail section.
+              <p className="mb-sm">Select an issue thumbnail photo.</p>
+              <p>
+                <span className="note">Note: </span>The issue thumbnail is the
+                cover photo for the issue. If you&#39;d like, you may reuse the
+                same thumbnail as the one used in the book thumbnail section.
+              </p>
             </li>
 
             <li className="bold-marker">
-              <strong className="mid-bold">Add an Issue description.</strong>{" "}
-              Add a description of the current issue rather than the description
-              of the entire book.
+              <p className="mb-sm">Add an Issue description.</p>
+              <p>
+                Add a description of the current issue rather than the
+                description of the entire book.
+              </p>
             </li>
 
             <li className="bold-marker">
-              <strong className="mid-bold">
-                Drag &#39;n&#39; Drop all the Issue pages
-              </strong>{" "}
-              or select the box and select all the necessary files.{" "}
-              <strong className="mid-bold">
-                The recommended file width is 1988 pixels
-              </strong>{" "}
-              and{" "}
-              <strong className="mid-bold">
-                the recommended height is 3056 pixels
-              </strong>{" "}
-              for all the uploaded issue pages. The width and height can be
-              smaller, but no larger than the recommended file dimensions. The
-              file size of any given page can be{" "}
-              <strong className="mid-bold">
-                no larger than 2 megabytes. If the pages appear in the incorrect
-                order, Drag &#39;n&#39; Drop them to their correct position.
-              </strong>
+              <p className="mb-sm">
+                Drag &#39;n&#39; Drop all the Issue pages or select the box and
+                select all the necessary files.
+              </p>
+              <p className="mb-sm">
+                <span className="note">Note: </span>
+                The recommended file width is 1988 pixels and the recommended
+                height is 3056 pixels for all the uploaded issue pages. The
+                width and height can be smaller, but no larger than the
+                recommended file dimensions.
+              </p>
+              <p className="mb-sm">
+                <span className="note">Note 2: </span>
+                The file size of any given page can be no larger than 2
+                megabytes.
+              </p>
+              <p>
+                <span className="note">Note 3: </span>
+                If the pages appear in the incorrect order, Drag &#39;n&#39;
+                Drop them to their correct position.
+              </p>
             </li>
-
             <li className="bold-marker">
-              <div className="inline-li">
-                <strong className="mid-bold">
-                  Give work credit(s) to all the users that assisted in creating
-                  the first issue (including yourself).
-                </strong>{" "}
+              <p className="mb-sm">
+                Give work credit(s) to all the users that assisted in creating
+                the first issue (including yourself).
+              </p>
+
+              <p className="mb-sm">
                 Notice your username is already present in this section.
-              </div>
-              <div className="mt-sm">
+              </p>
+
+              <p className="mb-sm">
+                <span className="note">Note: </span>
                 <strong className="mid-bold">
                   If you are the only person who assisted in creating this work,
                 </strong>{" "}
-                Simply selecting the{" "}
-                <strong className="mid-bold">&#34;writer&#34;</strong> and{" "}
-                <strong className="mid-bold">&#34;artist&#34;</strong>{" "}
-                checkboxes will suffice. There is no reason to select all the
-                available checkboxes, as it will overcomplicate the search for
-                users trying to view{" "}
-                <strong className="mid-bold">
-                  which primary roles were fulfilled.
-                </strong>
-              </div>
-              <div className="mt-sm">
+                Simply selecting the &#34;writer&#34; and &#34;artist&#34;
+                checkboxes will suffice.{" "}
+              </p>
+              <p className="mb-sm">
+                <strong className="mid-bold">Important!</strong> There is no
+                reason to select all the available checkboxes, as it will
+                overcomplicate the search for users trying to view which primary
+                roles were fulfilled.
+              </p>
+
+              <p className="mb-sm">
+                <span className="note">Note 2: </span>
                 <strong className="mid-bold">
                   If other users assisted in creating this work
                 </strong>
                 , search and select their username. When the dropdown closes,
-                their name will appear at the bottom of the credits list.{" "}
+                their name will appear at the bottom of the credits list.
+              </p>
+              <p className="mb-lg">
+                <strong className="mid-bold">Important!</strong> Again, there is
+                no need to select all the available checkboxes as it may
+                overcomplicate the search later. Just select the primary roles
+                each user has fulfilled in the issues&#39; creation.
+              </p>
+
+              <p>
                 <strong className="mid-bold">
-                  Again, there is no need to select all the available checkboxes
-                  as it may overcomplicate the search later. Just select the
-                  primary roles each user has fulfilled in the issues&#39;
-                  creation.
+                  Only the user who uploaded the comic book can change the work
+                  credits in the future.
                 </strong>
-              </div>
-              <div className="mt-sm">
-                <strong className="mid-bold">Important!</strong> Only the user
-                who uploaded the comic book can change the work credits in the
-                future.
-              </div>
+              </p>
             </li>
             <li className="bold-marker">
-              <strong className="mid-bold">Press the submit button.</strong>{" "}
-              This process might take a while, especially if the files you
-              provided were large. Try to be patient and avoid the temptation to
-              refresh. When the upload is complete, the page will redirect to
-              the details page of the newly published comic book.
+              <p className="mb-sm">Press the submit button.</p>
+              <p className="mb-sm">
+                This process might take a while, especially if the files you
+                provided were large. Try to be patient and avoid the temptation
+                to refresh.
+              </p>
+              <p>
+                When the upload is complete, the page will redirect to the
+                details page of the newly published comic book.
+              </p>
             </li>
           </ol>
         </article>
