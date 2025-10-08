fc955bfb80df8e687c4bfd0e99b4f1b0df88c34c
Simone Anthony <simone.anthony1@yahoo.com>
2025-10-07 22:04:46 -0400

    keys.js now accounts for local env

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,10 @@
 /* eslint-disable global-require */
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./prod');
-} else {
+} else if (process.env.NODE_ENV === 'development') {
   // eslint-disable-next-line node/no-unpublished-require
   module.exports = require('./dev');
+} else {
+  // eslint-disable-next-line node/no-unpublished-require
+  module.exports = require('./local');
 }
