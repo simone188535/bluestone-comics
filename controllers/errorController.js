ab8281bfb0da70777b35068207bd8236d2e5bd01
Simone Anthony <simone.anthony1@yahoo.com>
2025-10-14 22:47:46 -0400

    error controller now mentions process.env.NODE_ENV === 'local'

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -18,7 +18,10 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  if (process.env.NODE_ENV === 'development') {
+  if (
+    process.env.NODE_ENV === 'development' ||
+    process.env.NODE_ENV === 'local'
+  ) {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
     sendErrorProd(err, res);
