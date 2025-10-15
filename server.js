97a55dfa41a134116ab4d18ca8f1a934cdc71afc
Simone Anthony <simone.anthony1@yahoo.com>
2025-10-14 22:36:41 -0400

    config function moved to top of server file to avoid process.env.NODE_ENV from being undefined, sequelize commented out

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
-const dotenv = require('dotenv');
+// This enables ENV Variables
+require('dotenv').config({ path: './config.env' });
 // const mongoose = require('mongoose');
 // const keys = require('./config/keys');
-const Pool = require('./db');
+const {
+  pool // sequelize
+} = require('./db');
 // This enables ENV Variables
-dotenv.config({ path: './config.env' });
 
-Pool.connect()
+// console.log(process.env.NODE_ENV);
+// console.log('keys', keys);
+
+// sequelize added for migrations and seeding data
+// try {
+//   const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname');
+//   await sequelize.authenticate();
+//   console.log('Connection has been established successfully.');
+// } catch (error) {
+//   console.error('Unable to connect to the database:', error);
+// }
+
+pool
+  .connect()
   .then(() => console.log('PG DB connection successful!'))
   .catch((err) => {
     console.log('PG DB connection failed!', err);
-    Pool.end();
+    pool.end();
   });
 
 // mongoose
