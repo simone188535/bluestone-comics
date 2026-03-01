e86cb851848d084ec5c97fa2b672feb089b1f6ef
Simone Anthony <simone.anthony1@yahoo.com>
2026-03-01 03:53:11 -0500

    postgres updated from 13.20 to 18.x, psql edits added to account for ssl requirements locally and in prod

--- db/index.js
+++ db/index.js
@@ -1,9 +1,18 @@
 const { Pool } = require('pg');
+const fs = require('fs');
 const { Sequelize } = require('sequelize');
 const keys = require('../config/keys');
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const pool = new Pool({
-  connectionString: keys.PG_URI
+  connectionString: keys.PG_URI,
+  ssl: isProd
+    ? { rejectUnauthorized: true } // prod relies on system trust store OR env-provided CA
+    : {
+        rejectUnauthorized: true,
+        ca: fs.readFileSync(keys.CERTIFICATE_AUTHORITY, 'utf8')
+      }
 });
 
 const sequelize = new Sequelize(keys.PG_URI);
